36b0d0922b907a3fd1200bb33b75983e67683ae4
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-04-17 22:41:58 +0530

    added dates fields

--- retail_datawarehouse/models/intermediate/int_dim_dates.sql
+++ retail_datawarehouse/models/intermediate/int_dim_dates.sql
@@ -9,9 +9,13 @@ WITH raw_dates AS (
 SELECT
     invoice_date AS date_id,
     EXTRACT(DATE FROM invoice_date) AS date,
-    EXTRACT(DAYOFWEEK FROM invoice_date) AS weekday,
+    EXTRACT(DAYOFWEEK FROM invoice_date) AS week_number,
+    DATE_TRUNC(DATE(invoice_date), week) AS week,
     EXTRACT(YEAR FROM invoice_date) AS year,
-    EXTRACT(MONTH FROM invoice_date) AS month,
+    EXTRACT(MONTH FROM invoice_date) AS month_no,
+    FORMAT_DATETIME('%B', invoice_date) AS month,
+    EXTRACT(HOUR FROM invoice_date) AS hour,
+    EXTRACT(MINUTE FROM invoice_date) AS minute,
     EXTRACT(QUARTER FROM invoice_date) AS quarter,
     FORMAT_DATETIME('%m-%Y', invoice_date) AS month_year,
     FORMAT_DATETIME('%B-%Y', invoice_date) AS month_year_full
