a863c6fa725c0ceb6b259aff36ec6b97fd9755f7
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-04-18 21:55:40 +0530

    refactored code

--- retail_datawarehouse/models/intermediate/int_dim_customers.sql
+++ retail_datawarehouse/models/intermediate/int_dim_customers.sql
@@ -2,13 +2,16 @@
 
 WITH customer_dim AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['Customer_id', 'Country']) }} as customer_id,
+        {{ dbt_utils.generate_surrogate_key(['customer_id', 'country']) }} as customer_id,
         country
     FROM
         {{ ref('stg_retail') }}
+    WHERE
+        customer_id IS NOT NULL
 )
 
 SELECT
-    *
+    DISTINCT customer_id,
+    country
 FROM
     customer_dim
