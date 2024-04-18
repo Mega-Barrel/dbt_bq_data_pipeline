35f14be60a47c8bb96af0ef0d98c25db26f3a439
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-04-18 21:56:36 +0530

    added description column

--- retail_datawarehouse/models/staging/stg_retail.sql
+++ retail_datawarehouse/models/staging/stg_retail.sql
@@ -1,14 +1,9 @@
 
-{{
-  config(
-    materialized = 'view',
-    )
-}}
-
 WITH test_data AS (
     SELECT
         InvoiceNo AS invoice_no,
         StockCode AS stock_code,
+        Description AS description,
         Quantity AS quantity,
         InvoiceDate AS invoice_date,
         UnitPrice AS unit_price,
